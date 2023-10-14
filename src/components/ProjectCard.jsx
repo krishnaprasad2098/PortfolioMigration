bcfc384d00b0f2863384eca976446d569f17a873
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-14 20:17:49 +0530

    Enhanced Projectcard layout

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,17 +3,22 @@ import React from "react";
 import Ethnic from "../assets/Ethnic.avif";
 
 const ProjectCard = (props) => {
-  const { imgname, projecttitle, projectdescription, projectlink } = props;
+  const { imgname, projecttitle, projectdescription, projectlink, id } = props;
   return (
-    <div className=" border-2 border-black mx-auto rounded-md">
-      <div className="xl:max-w-xs">
+    <div
+      className=" border-2 border-black mx-auto rounded-md bg-transparent max-h-screen"
+      key={id}
+    >
+      <div className="xl:max-w-xs flex flex-col h-full">
         <img
           src={imgname}
           alt={imgname}
           className="p-3 rounded-[1rem] object-contain"
         />
-        <p className="font-bold text-xl px-3 text-center">{projecttitle}</p>
-        <p className="font-semibold text-lg text-black px-3">
+        <p className="font-bold text-xl px-3 py-2 text-center">
+          {projecttitle}
+        </p>
+        <p className="font-semibold text-center text-lg text-black px-3 flex-grow ">
           {projectdescription}
         </p>
         <div className="grid place-items-center py-3">
