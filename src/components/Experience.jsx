01cbc260eca2ab96ac4f47c5851162654a420ac7
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-15 21:37:21 +0530

    Added Experience Section.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ExperienceData from "../data/ExperienceData";
+import ExperienceCard from "./ExperienceCard";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div>
+      Experience
+      <ExperienceCard />
+    </div>
+  );
 };
 
 export default Experience;
