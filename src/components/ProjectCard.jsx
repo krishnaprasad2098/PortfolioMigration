ed27faffe28d9305e14ab52b2efb79f8372b8ff0
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-15 21:36:57 +0530

    Updated styles.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-import Ethnic from "../assets/Ethnic.avif";
-
 const ProjectCard = (props) => {
   const { imgname, projecttitle, projectdescription, projectlink, id } = props;
   return (
     <div
-      className=" border-2 border-black mx-auto rounded-md bg-transparent max-h-screen"
+      className=" border-4 border-[#fccb06] mx-auto rounded-md   bg-[#222e50] "
       key={id}
     >
-      <div className="xl:max-w-xs flex flex-col h-full">
+      <div className="xl:max-w-xs flex flex-col h-full text-white ">
         <img
           src={imgname}
           alt={imgname}
@@ -18,13 +16,13 @@ const ProjectCard = (props) => {
         <p className="font-bold text-xl px-3 py-2 text-center">
           {projecttitle}
         </p>
-        <p className="font-semibold text-center text-lg text-black px-3 flex-grow ">
+        <p className="font-semibold text-center text-lg text-white px-3 flex-grow ">
           {projectdescription}
         </p>
         <div className="grid place-items-center py-3">
           <button
             type="button"
-            className="px-10 py-3 bg-blue-300 border-2 font-semibold text-lg border-blue-600 rounded-md"
+            className="px-10 py-3 bg-[#edf7f6]  border-4 font-semibold text-lg text-black border-[#fccb06] rounded-md"
           >
             View in Github
           </button>
