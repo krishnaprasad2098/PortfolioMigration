fa219023eacff11dba28f52b5c01991608a4e0ba
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-15 21:37:57 +0530

    Added ExperienceSection Data.

---
+++ src/data/ExperienceData.js
@@ -0,0 +1,20 @@
+const ExperienceData = [
+  {
+    id: 1,
+    company: "Limra United Technologies",
+    title: "Automation Tester Intern",
+    duration: "Sep 2022 to May 2023",
+    description: "",
+    icon: "work",
+  },
+  {
+    id: 2,
+    company: "Prodapt",
+    title: "Project Intern",
+    duration: "Sep 2023 to Feb 2024",
+    decription: "",
+    icon: "work",
+  },
+];
+
+export default ExperienceData;
