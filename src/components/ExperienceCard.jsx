92ee4a34bd4feb5c8b4094850d2cc233aa3b345e
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-16 21:43:08 +0530

    Added Experience card

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,39 +1,40 @@
 import React from "react";
 import ExperienceData from "../data/ExperienceData";
-import { Icon } from "@iconify/react";
 
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-const ExperienceCard = () => {
-  const workIconStyles = { background: "#06D6A0" };
+const ExperienceCard = (props) => {
   return (
     <div>
-      <VerticalTimeline>
-        {ExperienceData.map((exe) => {
-          // const IsWorkIcon = exe.icon === "work";
-          return (
-            <VerticalTimelineElement
-              key={exe.id}
-              date={exe.duration}
-              dateClassName="date"
-              iconStyle={exe.icon === "work" ? workIconStyles : null}
-              icon={<Icon icon="ph:desktop-duotone" />}
-            >
-              <div className="flex flex-col gap-0">
-                <h1 className="vertical-timeline-element-title font-bold">
-                  {exe.company}
-                </h1>
-                <p className="vertical-timeline-element-subtitle">
-                  {exe.title}
+      <div
+        className="xl:max-w-xl h-full flex flex-col gap-1 border-4 border-x-purple-500 border-y-violet-500 rounded-xl px-3"
+        key={props.id}
+      >
+        <img
+          src={props.logo}
+          alt={props.company}
+          className="mix-blend-multiply max-w-[8rem] mx-auto"
+        />
+        <p className="text-xl font-bold text-center">{props.company}</p>
+        <p className="text-lg font-medium text-center">{props.title}</p>
+
+        <p className="text-base font-medium text-center">{props.duration}</p>
+        <p className="font-semibold text-justify font-sans">
+          {props.description}
+        </p>
+        <p className="font-bold text-center text-lg flex-grow">
+          Technical Skills
+        </p>
+        <div className="flex gap-3   py-2 flex-wrap justify-stretch ">
+          {props.technologies.map((tec, idx) => {
+            return (
+              <div className="py-1 xl:flex-grow text-center" key={idx}>
+                <p className="py-1 border-2 border-blue-500 px-5 text-black font-semibold text-base bg-purple-300 rounded-full">
+                  {tec.tech}
                 </p>
               </div>
-            </VerticalTimelineElement>
-          );
-        })}
-      </VerticalTimeline>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
