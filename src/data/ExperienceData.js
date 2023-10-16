4c99b7ac056a0359f836d5de86f773df59c909fa
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-16 21:43:42 +0530

    Added Experiencedata

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -1,19 +1,54 @@
+import Limra from "../assets/Limra.jpeg";
+
 const ExperienceData = [
   {
     id: 1,
     company: "Limra United Technologies",
     title: "Automation Tester Intern",
     duration: "Sep 2022 to May 2023",
-    description: "",
-    icon: "work",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facilis aspernatur odio, excepturi non perferendis ducimus culpa voluptates libero aliquam ea, laborum quae quidem animi debitis fuga labore consectetur numquam.",
+    technologies: [
+      {
+        tech: "Cypress",
+      },
+      {
+        tech: "Typescript",
+      },
+      {
+        tech: "Selenium",
+      },
+      {
+        tech: "Java",
+      },
+      {
+        tech: "MySQL",
+      },
+    ],
+    logo: Limra,
   },
   {
     id: 2,
     company: "Prodapt",
     title: "Project Intern",
     duration: "Sep 2023 to Feb 2024",
-    decription: "",
-    icon: "work",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facilis aspernatur odio, excepturi non perferendis ducimus culpa voluptates libero aliquam ea, laborum quae quidem animi debitis fuga labore consectetur numquam.",
+    technologies: [
+      {
+        tech: "Nuxt JS",
+      },
+      {
+        tech: "Typescript",
+      },
+      {
+        tech: "Tailwind CSS",
+      },
+      {
+        tech: "Python",
+      },
+    ],
+    logo: Limra,
   },
 ];
 
