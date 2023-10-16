f9e6083a0b9c318f7817897387d18c678768f723
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-16 21:42:49 +0530

    added spacing in y direction

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import ProjectData from "../data/ProjectData";
 
 const Projects = () => {
   return (
-    <div className="">
+    <div className="py-10">
       <h1 className="text-5xl font-bold py-10 text-center ">Projects</h1>
       <div className="gap-2 grid sm:items-stretch md:grid-cols-2 xl:grid-cols-3 sm:gap-5 md:gap-3 sm:grid-cols-2 grid-cols-1 xl:max-w-max xl:mx-auto px-4 xl:gap-16">
         {ProjectData.map((project) => {
