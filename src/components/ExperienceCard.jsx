50c518aa9fbf6179eca47c34eea8e5279f0f1e02
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-17 19:55:43 +0530

    fixed layout issues.

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -8,21 +8,21 @@ const ExperienceCard = (props) => {
         className="xl:max-w-xl h-full flex flex-col gap-1 border-4 border-x-purple-500 border-y-violet-500 rounded-xl px-3"
         key={props.id}
       >
-        <img
-          src={props.logo}
-          alt={props.company}
-          className="mix-blend-multiply max-w-[8rem] mx-auto"
-        />
+        <div className="pt-5 flex items-center justify-center flex-grow">
+          <img
+            src={props.logo}
+            alt={props.company}
+            className=" max-w-[8rem] m-auto object-center "
+          />
+        </div>
         <p className="text-xl font-bold text-center">{props.company}</p>
-        <p className="text-lg font-medium text-center">{props.title}</p>
+        <p className="text-lg font-normal text-center">{props.title}</p>
 
         <p className="text-base font-medium text-center">{props.duration}</p>
-        <p className="font-semibold text-justify font-sans">
+        <p className="font-semibold text-justify font-sans ">
           {props.description}
         </p>
-        <p className="font-bold text-center text-lg flex-grow">
-          Technical Skills
-        </p>
+        <p className="font-bold text-center text-lg ">Technical Skills</p>
         <div className="flex gap-3   py-3 flex-wrap justify-stretch ">
           {props.technologies.map((tec, idx) => {
             return (
