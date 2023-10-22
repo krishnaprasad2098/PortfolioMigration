232364d69a2ea8a99586af2432357298a508fc73
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-22 12:54:15 +0530

    updated skilldata

---
+++ src/data/SkillData.js
@@ -0,0 +1,64 @@
+import html from "../assets/html.png";
+import react from "../assets/React.svg";
+import cypress from "../assets/Cypress.svg";
+import git from "../assets/Git.svg";
+import java from "../assets/Java.svg";
+import javascript from "../assets/Javascript.svg";
+import typescript from "../assets/Typescript.svg";
+import nuxt from "../assets/Nuxt.svg";
+import mysql from "../assets/MySQL.svg";
+import python from "../assets/Python.svg";
+import tailwindcss from "../assets/tailwindcss.svg";
+import selenium from "../assets/Selenium.svg";
+const SkillData = [
+  {
+    id: 1,
+    img: react,
+  },
+  {
+    id: 2,
+    img: nuxt,
+  },
+  {
+    id: 3,
+    img: typescript,
+  },
+  {
+    id: 4,
+    img: javascript,
+  },
+  {
+    id: 5,
+    img: python,
+  },
+  {
+    id: 6,
+    img: java,
+  },
+  {
+    id: 7,
+    img: selenium,
+  },
+  {
+    id: 8,
+    img: cypress,
+  },
+  {
+    id: 9,
+    img: mysql,
+  },
+  {
+    id: 10,
+    img: tailwindcss,
+  },
+  {
+    id: 11,
+    img: html,
+  },
+  {
+    id: 12,
+    img: git,
+  },
+];
+
+export default SkillData;
