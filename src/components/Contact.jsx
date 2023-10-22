2232f1593254c64c92a16bb936eb283bb89ae8d8
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-10-22 21:25:48 +0530

    updated contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Form from "../components/Form";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1 className="text-5xl font-semibold text-center py-7">Reach Me Out</h1>
+      <Form />
+    </div>
+  );
 };
 
 export default Contact;
