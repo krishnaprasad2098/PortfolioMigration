2da21e20f1e78ea544e4b82231a56601be9a5232
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-11-20 19:14:34 +0530

    updated experience section data.

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -4,11 +4,34 @@ import Prodapt from "../assets/Prodapt.png";
 const ExperienceData = [
   {
     id: 1,
+    company: "Prodapt",
+    title: "Project Intern",
+    duration: "Sep 2023 to Feb 2024",
+    description:
+      "In my current role, I focus on front-end development, crafting UIs and reusable components. I rely on Nuxt as the framework, TailwindCSS for styling, and HeadlessUI for streamlined component creation. This combination allows for efficient and scalable UI development, enhancing user experiences seamlessly.",
+    technologies: [
+      {
+        tech: "Nuxt JS",
+      },
+      {
+        tech: "Typescript",
+      },
+      {
+        tech: "Tailwind CSS",
+      },
+      {
+        tech: "Python",
+      },
+    ],
+    logo: Prodapt,
+  },
+  {
+    id: 2,
     company: "Limra United Technologies",
     title: "Automation Tester Intern",
     duration: "Sep 2022 to May 2023",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facilis aspernatur odio, excepturi non perferendis ducimus culpa voluptates libero aliquam ea, laborum quae quidem animi debitis fuga labore consectetur numquam.",
+      "During my internship, I specialized in automation testing, leveraging Cypress and TypeScript extensively to test their legacy applications. I also delved into Selenium with Java, gaining valuable hands-on experience in both frameworks.",
     technologies: [
       {
         tech: "Cypress",
@@ -27,29 +50,6 @@ const ExperienceData = [
       },
     ],
     logo: Limra,
-  },
-  {
-    id: 2,
-    company: "Prodapt",
-    title: "Project Intern",
-    duration: "Sep 2023 to Feb 2024",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facilis aspernatur odio, excepturi non perferendis ducimus culpa voluptates libero aliquam ea, laborum quae quidem animi debitis fuga labore consectetur numquam.",
-    technologies: [
-      {
-        tech: "Nuxt JS",
-      },
-      {
-        tech: "Typescript",
-      },
-      {
-        tech: "Tailwind CSS",
-      },
-      {
-        tech: "Python",
-      },
-    ],
-    logo: Prodapt,
   },
 ];
 
