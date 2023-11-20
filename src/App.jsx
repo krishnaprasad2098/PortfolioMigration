3a5012cf691c3ec0c70c7b1e3655206957dce0dc
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-11-20 19:17:13 +0530

    added section,smooth scroll.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { useState } from "react";
 import Navbar from "./components/Navbar.jsx";
 import MainSection from "./components/MainSection.jsx";
 import Experience from "./components/Experience.jsx";
 import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
 import Footer from "./components/Footer.jsx";
-import "./App.css";
+
+import AboutMe from "./components/AboutMe.jsx";
+import Skills from "./components/Skills.jsx";
 
 function App() {
   return (
-    <div className="space-y-4">
+    <div className="">
       <Navbar />
       <MainSection />
-      <Experience />
-      <Projects />
-      <Contact />
+      <section id="AboutMe">
+        <AboutMe />
+      </section>
+      <section id="Experience">
+        <Experience />
+      </section>
+      <section id="Skills">
+        <Skills />
+      </section>
+      <section id="Projects">
+        <Projects />
+      </section>
+      <section id="Contact">
+        <Contact />
+      </section>
+
       <Footer />
     </div>
   );
