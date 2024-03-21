dc1cecceddc7a79afce0b125009071222de02304
krishnaprasad2098 <payamma.krishna@gmail.com>
2024-03-21 18:29:57 +0530

    add project links

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -10,7 +10,7 @@ const ProjectData = [
     projecttitle: "Feedback System",
     projectdescription:
       "This is a web application for collecting feedback from students on faculty.This was built using ReactJS,ChartJS and Supabase.",
-    projectlink: "https://github.com/krishnaprasad2098",
+    projectlink: "https://github.com/krishnaprasad2098/feedback-system",
   },
   {
     id: 2,
@@ -18,7 +18,7 @@ const ProjectData = [
     projecttitle: "Ethnic",
     projectdescription:
       "An E-Commerce application for handicraft sellers to improve their  sales.This was Built using ReactJS and Firebase.",
-    projectlink: "https://github.com/krishnaprasad2098",
+    projectlink: "https://github.com/krishnaprasad2098/Ethinic",
   },
   {
     id: 3,
@@ -34,7 +34,7 @@ const ProjectData = [
     projecttitle: "Gym Landing Page",
     projectdescription:
       "This is a landing page of a gym to attract customers and improve their business and market it.This website was built using ReactJS and Tailwind CSS.",
-    projectlink: "https://github.com/krishnaprasad2098",
+    projectlink: "https://github.com/krishnaprasad2098/bro_gym",
   },
   {
     id: 5,
